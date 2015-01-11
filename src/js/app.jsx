f8f300b30f4670dacda25d204d74deed2c11a120
Victor Bjelkholm <victorbjelkholm@gmail.com>
2015-01-11 15:08:50 +0100

    Dynamic tld, http-server included, build on cmd

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,7 +3,17 @@
 // Development
 //window.serverUrl = "http://localhost:8001/";
 // Production
-window.serverUrl = "http://api.headlinie.com/";
+var hostSplitted = window.location.host.split('.');
+var domain;
+var tld;
+if(hostSplitted.length !== 2) {
+  var domain = hostSplitted[1];
+  var tld = hostSplitted[2];
+} else {
+  var domain = hostSplitted[0];
+  var tld = hostSplitted[1];
+}
+window.serverUrl = "http://api."+domain+"."+tld+"/";
 
 // Locking to portrait if possible
 function lockToPortait(){
@@ -23,11 +33,9 @@ lockToPortait();
 // Development
 //window.manifestUrl = 'http://localhost:8000/manifest.webapp';
 // Production
-window.manifestUrl = 'http://headlinie.com/manifest.webapp';
+window.manifestUrl = 'http://'+domain+'.'+tld+'/manifest.webapp';
 
 //Initialization for the application
 var startApplication = require('./load_page.js');
-
-console.log(startApplication)
 
 startApplication('WorldNews');
